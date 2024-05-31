43dad6ba1f25d9320329c508af76ccc637fbaa62
yiixfeng <1920131449@qq.com>
2024-05-31 11:45:40 +0800

    Create anniu.js

---
+++ js/anniu.js
@@ -0,0 +1,8 @@
+  function showPopup(){
+    var overlay = document.getElementById("overlay");
+    overlay.style.display = "block";
+  }
+  function hidePopup(){
+    var overlay = document.getElementById("overlay");
+    overlay.style.display = "none";
+  }
